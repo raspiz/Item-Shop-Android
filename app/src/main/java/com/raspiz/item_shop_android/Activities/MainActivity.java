e20efc9df4b274e443a50989b0418710be1e7f26
Aaron Whitmer <a.w.whitmer@eagle.clarion.edu>
2016-04-09 13:40:16 -0400

    initial commit

---
+++ app/src/main/java/com/raspiz/item_shop_android/Activities/MainActivity.java
@@ -0,0 +1,15 @@
+package com.raspiz.item_shop_android.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.raspiz.item_shop_android.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/raspiz/item_shop_android/DataObjects/Item.java
@@ -0,0 +1,31 @@
+package com.raspiz.item_shop_android.DataObjects;
+
+/**
+ * Created by Aaron on 4/9/2016.
+ */
+public class Item {
+
+    private class Info {
+        private String name;
+        private String subname;
+        private String primary;
+        private String secondary;
+        private Integer basePrice;
+        private String tagOne;
+        private String tagTwo;
+        private String masterCat;
+        private String mainCat;
+        private String subCat;
+        private Integer catId;
+        private String multiplier;
+        private Integer tier;
+        private Integer mCatId;
+        private Integer sCatId;
+        private Integer itemId;
+
+
+
+    }
+
+
+}
